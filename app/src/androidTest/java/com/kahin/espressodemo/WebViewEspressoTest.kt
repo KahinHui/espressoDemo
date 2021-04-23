b897d19164fc8e6a2619b16bddd39e042e5fc41b
许嘉轩（990029） <jiaxuanxu@fcbox.com>
2021-04-23 14:27:58 +0800

    feature: complete espresso test webView

--- app/src/androidTest/java/com/kahin/espressodemo/WebViewEspressoTest.kt
+++ app/src/androidTest/java/com/kahin/espressodemo/WebViewEspressoTest.kt
@@ -1,13 +1,15 @@
 package com.kahin.espressodemo
 
-import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.web.assertion.WebViewAssertions.webMatches
+import androidx.test.espresso.web.model.Atoms
 import androidx.test.espresso.web.sugar.Web.onWebView
-import androidx.test.espresso.web.webdriver.DriverAtoms
 import androidx.test.espresso.web.webdriver.DriverAtoms.*
 import androidx.test.espresso.web.webdriver.Locator
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.kahin.espressodemo.ui.main.activity.WebViewActivity
+import org.hamcrest.core.StringContains.containsString
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -17,22 +19,33 @@ class WebViewEspressoTest {
     @get:Rule
     val callActivityRule = ActivityScenarioRule(WebViewActivity::class.java)
 
+    @Before
+    fun startUp() {
+        onWebView().forceJavascriptEnabled()
+    }
+
     @Test
     fun typeTextInInput_clickButton_SubmitsForm() {
-        onWebView().forceJavascriptEnabled()
-
         // Selects the WebView in your layout. If you have multiple WebViews you can also use a
         // matcher to select a given WebView, onWebView(withId(R.id.web_view)).
         onWebView()
-                // Find the input element by ID
-                .withElement(findElement(Locator.ID, "sb_form_q"))
-                // Clear previous input
-                .perform(clearElement())
-                // Enter text into the input element
-                .perform(DriverAtoms.webKeys("hello"))
-                // Find the submit button
-                .withElement(findElement(Locator.ID, "sb_form_go"))
-                // Simulate a click via javascript
-                .perform(webClick())
+            // Find the input element by ID
+            .withElement(findElement(Locator.ID, "sb_form_q"))
+            // Clear previous input
+            .perform(clearElement())
+            // Enter text into the input element
+            .perform(webKeys(HELLO))
+            // Find the submit button
+            .withElement(findElement(Locator.ID, "sbBtn"))
+            // Simulate a click via javascript
+            .perform(webClick())
+            .withElement(findElement(Locator.ID, "sb_form_q"))
+            // Similar to check(matches(...))
+            .check(webMatches(Atoms.getCurrentUrl(), containsString(HELLO)))
+            .reset()
+    }
+
+    companion object {
+        const val HELLO = "hello"
     }
 }
